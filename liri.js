d89885e377b16aeb00f9519f74ce02abbc049ad3
Eileen S <eileen.j.sul@gmail.com>
2018-12-15 21:53:13 -0600

    Implemented artist

--- liri.js
+++ liri.js
@@ -1,20 +1,49 @@
 require("dotenv").config();
 
-// var keys = require("./keys.js");
+const keys = require("./keys.js");
 
-// var Spotify = require('node-spotify-api');
+const Spotify = require('node-spotify-api');
+const axios = require('axios')
+const moment = require('moment')
  
-// var spotify = new Spotify({
-//   id: <your spotify client id>,
-//   secret: <your spotify client secret>
-// });
- 
+const spotify = new Spotify({
+  id: keys.spotify.id,
+  secret: keys.spotify.secret
+});
+
+function searchArtist(artist) {
+    axios.get(`https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`).then((response) => {
+        for(let i = 0; i < response.data.length; ++i) {
+            console.log('-------------------------------')
+            const event = response.data[i]
+            console.log(`Event venue name: ${event.venue.name}`)
+            console.log(`Event city: ${event.venue.city}`)
+            const eventDate = moment(event.datetime).format('MM/DD/YYYY')
+            console.log(`Event date: ${eventDate}`)
+        }
+    })
+}
 // spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-//   if (err) {
-//     return console.log('Error occurred: ' + err);
-//   }
- 
-// // console.log(data); 
-// // });
+//     if (err) {
+//         return console.log('Error occurred: ' + err);
+//     }
+//     console.log(data); 
+// });
 
 // https://rest.bandsintown.com/artists/temptations/events?app_id=codingbootcamp
+
+const command = process.argv[2]
+
+if(command === 'concert-this') {
+    searchArtist(process.argv[3])
+}
+if(command === 'spotify-this-song') {
+
+}
+if(command === 'movie-this') {
+
+}
+if(command === 'do-what-it-says') {
+
+}
+
